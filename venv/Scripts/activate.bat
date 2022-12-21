646b01a841b5289993021d2e04d46ad12a8c08b3
httpsGabrielle <gabrielleso125@gmail.com>
2022-12-20 22:25:19 -0300

    add contatos

--- venv/Scripts/activate.bat
+++ venv/Scripts/activate.bat
@@ -8,7 +8,7 @@ if defined _OLD_CODEPAGE (
     "%SystemRoot%\System32\chcp.com" 65001 > nul
 )
 
-set VIRTUAL_ENV=C:\Users\ALUNO_11\Desktop\Estagiaria\culturaDigital\venv
+set VIRTUAL_ENV=C:\Users\Gabe\culturaDigital\venv
 
 if not defined PROMPT set PROMPT=$P$G
 
